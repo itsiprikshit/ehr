62c0df90f56491abf57a6c3a86080f4ccca96d4d
Prikshit <prikshit911@gmail.com>
2015-02-27 01:48:20 +0530

    changed db name

--- includes/config.php
+++ includes/config.php
@@ -4,6 +4,6 @@ define("conn_error","Could not connect !");
 define("mysql_host",getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define("mysql_user",getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define("mysql_pass",getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define("mysql_db","ehr");
+define("mysql_db","medical");
 
 ?>
